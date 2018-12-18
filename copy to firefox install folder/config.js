7ed0b2710942289af8daf871dd7aa87d8bb00a73
Alex Mehler <alexmehler@t-online.de>
2018-12-18 16:17:47 +0100

    Add files via upload
    new version

--- copy to firefox install folder/config.js
+++ copy to firefox install folder/config.js
@@ -1,164 +1,112 @@
 //
-/* ***** BEGIN LICENSE BLOCK *****
- * Version: MPL 1.1/GPL 2.0/LGPL 2.1
- *
- * The contents of this file are subject to the Mozilla Public License Version
- * 1.1 (the "License"); you may not use this file except in compliance with
- * the License. You may obtain a copy of the License at
- * http://www.mozilla.org/MPL/
- *
- * Software distributed under the License is distributed on an "AS IS" basis,
- * WITHOUT WARRANTY OF ANY KIND, either express or implied. See the License
- * for the specific language governing rights and limitations under the
- * License.
- *
- * The Original Code is the userChrome.js component.
- *
- * The Initial Developer of the Original Code is
- * Simon Bünzli <zeniko@gmail.com>
- *
- * Portions created by the Initial Developer are Copyright (C) 2007
- * the Initial Developer. All Rights Reserved.
- *
- * Contributor(s):
- * alta88 <alta88@gmail.com>
- *
- * Alternatively, the contents of this file may be used under the terms of
- * either the GNU General Public License Version 2 or later (the "GPL"), or
- * the GNU Lesser General Public License Version 2.1 or later (the "LGPL"),
- * in which case the provisions of the GPL or the LGPL are applicable instead
- * of those above. If you wish to allow use of your version of this file only
- * under the terms of either the GPL or the LGPL, and not to allow others to
- * use your version of this file under the terms of the MPL, indicate your
- * decision by deleting the provisions above and replace them with the notice
- * and other provisions required by the GPL or the LGPL. If you do not delete
- * the provisions above, a recipient may use your version of this file under
- * the terms of any one of the MPL, the GPL or the LGPL.
- *
- * ***** END LICENSE BLOCK ***** */
-
-try {
-
-/*Components.utils.import("resource://gre/modules/XPCOMUtils.jsm");
-*/
+// config.js userchrome loader
+Components.utils.import("resource://gre/modules/Services.jsm");
 Components.utils.import("resource://gre/modules/osfile.jsm");
+Components.utils.import("resource://gre/modules/FileUtils.jsm");
+Components.utils.import("resource://gre/modules/XPCOMUtils.jsm");
+Components.utils.import("resource://gre/modules/Console.jsm");
+const directory_service = Components.classes["@mozilla.org/file/directory_service;1"].getService(Components.interfaces.nsIProperties);
 
-function UserChrome_js() {
-  var os = Components.classes["@mozilla.org/observer-service;1"]
-                     .getService(Components.interfaces.nsIObserverService);
-  os.addObserver(this, "final-ui-startup", false);
-};
+let userChromeLoader = {
+	init: function () {
+		// CANCEL IN SAFEMODE
+		if (Components.classes['@mozilla.org/xre/app-info;1'].getService(Components.interfaces.nsIXULRuntime).inSafeMode)
+			return;
 
-UserChrome_js.prototype = {
-/*// Properties required for XPCOM registration:
-  classDescription: "userChromeJS Loading Component",
-  classID         : Components.ID("{8DEB3B5E-7585-4029-B6D0-4733CE8DED50}"),
-  contractID      : "@userChromeJS;1",
+		//  REGISTER CHROME FOLDER AS chrome://userchromejs/content/  ...this makes everything so easy :)
+		let cmanifest = directory_service.get('UChrm', Components.interfaces.nsIFile);
+		cmanifest.append('chrome.manifest');
+		Components.manager.QueryInterface(Components.interfaces.nsIComponentRegistrar).autoRegister(cmanifest);
 
-  _xpcom_categories: [{
-    category: "app-startup",
-    service: true
-  }],
-*/
-/* ........ QueryInterface .................................................. */
-/*
-  QueryInterface: XPCOMUtils.generateQI([Components.interfaces.nsISupports,
-                                         Components.interfaces.nsIObserver,
-                                         Components.interfaces.nsIModule,
-                                         Components.interfaces.nsIFactory,
-                                         Components.interfaces.nsIDOMEventListener]),
-*/
-/* ........ nsIObserver ..................................................... */
+		//IMPORT FILES FROM FOLDERS
+		let legacyScripts = this.importFolder(FileUtils.getDir("ProfD", ["chrome", "legacy"], true));
+		let sandboxedScripts = this.importFolder(FileUtils.getDir("ProfD", ["chrome", "sandboxed"], true));
+		let overlays = this.importFolder(FileUtils.getDir("ProfD", ["chrome","overlays"], true));
 
-  observe: function(aSubject, aTopic, aData) {
-    var os = Components.classes["@mozilla.org/observer-service;1"]
-                       .getService(Components.interfaces.nsIObserverService);
+		//RUN LEGACY SCRIPTS
+		let enumerator = Services.wm.getEnumerator("navigator:browser");
+		while (enumerator.hasMoreElements()) {
+			let win = enumerator.getNext();
+			if (win.PrivateBrowsingUtils.isWindowPrivate(win) == false)
+				this.loadLegacyScripts(win.document, legacyScripts);
+		};
+		let windowStartUpObserver = {
+			observe: async function (aSubject, aTopic, aData) {
+				aSubject.addEventListener('DOMContentLoaded', function (aEvent) {
+					let document = aEvent.originalTarget;
+					userChromeLoader.loadLegacyScripts(document, legacyScripts);
+				}, true);
+			}
+		}
+		Services.obs.addObserver(windowStartUpObserver, 'domwindowopened', false);
 
-    switch (aTopic) {
-/*  case "app-startup":
-    case "profile-after-change":
-      os.addObserver(this, "final-ui-startup", false);
-      break;
-*/  case "final-ui-startup":
-      var file = Components.classes["@mozilla.org/file/directory_service;1"]
-                           .getService(Components.interfaces.nsIProperties)
-                           .get("UChrm", Components.interfaces.nsIFile);
-      file.append("userChrome.js");
-/*
-      if (!file.exists()) {
-        var componentFile = __LOCATION__;
-        var componentsDir = componentFile.parent;
-        var extensionDir = componentsDir.parent;
-        extensionDir.append("README.txt");
-        if (extensionDir.exists())
-          extensionDir.copyTo(file.parent, "userChrome.js");
-      }
-*/
-      if (file.exists() && file.isFile() &&
-          !Components.classes["@mozilla.org/xre/app-info;1"]
-                     .getService(Components.interfaces.nsIXULRuntime)
-                     .inSafeMode) {
-        this.mFileURL = Components.classes["@mozilla.org/network/io-service;1"]
-                                  .getService(Components.interfaces.nsIIOService)
-                                  .getProtocolHandler("file")
-                                  .QueryInterface(Components.interfaces.nsIFileProtocolHandler)
-                                  .getURLSpecFromFile(file);
-        var path = OS.Constants.Path.libDir;
-        path = OS.Path.join(path, "userChromeJS.js");
-        this.uCFileURI = OS.Path.toFileURI(path);
+		//RUN SANDBOXED/BOOTSTRAPPED SCRIPTS
+		this.loadedBootstrapSandboxes = {};
+		for (let file of sandboxedScripts) {
+			let chromeURI = 'chrome://userchromejs/content/sandboxed/' + file.leafName;
+			try {
+				this.loadSandboxedScript(chromeURI, file.leafName);
+			} catch (err) {
+				console.log(err);
+			}
+		}
+	},
+	importFolder: function (aFolder) {
+		let files = [];
+		let dir = aFolder.directoryEntries;
+		while (dir.hasMoreElements()) {
+			let file = dir.getNext().QueryInterface(Components.interfaces.nsIFile);
+			if (!file.isDirectory()) {
+				files.push(file);
+			}
+		}
+		return files;
+	},
+	loadLegacyScripts: function (document, legacyScripts) {
+		if (document.location && document.location.protocol == 'chrome:') {
+			for (let file of legacyScripts) {
+				let chromeURI = 'chrome://userchromejs/content/legacy/' + file.leafName;
+				try {
+					Services.scriptloader.loadSubScript(chromeURI, document.defaultView, 'UTF-8');
+				} catch (err) {
+					console.log(err);
+				}
+			}
+		}
+	},
+	loadSandboxedScript: function (uri, name) {
+		// CREATE SANBOX
+		let principal = Components.classes["@mozilla.org/systemprincipal;1"].createInstance(Components.interfaces.nsIPrincipal);
+		let sandbox = new Components.utils.Sandbox(principal, {
+				sandboxName: uri,
+				metadata: {
+					URI: uri
+				}
+			});
+		XPCOMUtils.defineLazyGetter(// Define a console
+			sandbox, "console",
+			() => new ConsoleAPI({
+				consoleID: "userChrome/" + name
+			}));
+		sandbox.__SCRIPT_URI_SPEC__ = uri;
+		Services.scriptloader.loadSubScript(uri, sandbox);
 
-        os.addObserver(this, "domwindowopened", false);
-      }
-      break;
-    case "domwindowopened":
-      aSubject.addEventListener("load", this, true);
-      break;
-    }
-  },
-
-/* ........ nsIDOMEventListener ............................................. */
-
-  handleEvent: function(aEvent) {
-    var document = aEvent.originalTarget;
-    if (document.location && document.location.protocol == "chrome:") {
-      try {
-        let loader = Components.classes["@mozilla.org/moz/jssubscript-loader;1"]
-                               .getService(Components.interfaces.mozIJSSubScriptLoader);
-/*
-        loader.loadSubScript("chrome://userChromeJS/content/userChromeJS.js",
-                             document.defaultView,
-                             "UTF-8");
-*/
-        loader.loadSubScript(this.uCFileURI,
-                             document.defaultView,
-                             "UTF-8");
-
-        loader.loadSubScript(this.mFileURL,
-                             document.defaultView,
-                             "UTF-8");
-      }
-      catch (ex) {
-        // script execution can be stopped with |throw "stop";|
-        if (ex !== "stop") {
-          Components.utils.reportError(ex);
-        }
-      }
-    }
-  }
-
-};
-
-/**
- * The following line is what XPCOM uses to create components. Each component
- * prototype must have a .classID which is used to create it.
- *
- * XPCOMUtils.generateNSGetFactory was introduced in Mozilla 2 (Firefox 4).
- */
-/*var NSGetFactory = XPCOMUtils.generateNSGetFactory([UserChrome_js]);
-*/
-new UserChrome_js();
-
-} catch(ex) {
-  Components.utils.reportError(ex);
-};
-
+		//STARTUP
+		let runStartup = function (sandbox, reason) {
+			try {
+				let startupFunction = sandbox["startup"];
+				if (!startupFunction) {
+					console.log("error: startup() function not present");
+				}
+				startupFunction.call(sandbox, "ADDON_ENABLE");
+			} catch (e) {}
+		}
+		runStartup(sandbox, "ADDON_ENABLE");
+		// SHUTDOWN
+		let test = sandbox["shutdown"];
+		if (test) {
+			this.loadedBootstrapSandboxes[uri] = sandbox;
+		}
+	}
+}
+userChromeLoader.init();
